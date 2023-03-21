c4de1f3b557be035d9a901af06b00d6f3f2b6b16
scokljat <selma.cokljat@hotmail.com>
2023-03-21 12:03:52 +0100

    Updated constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -22,40 +22,4 @@ export const navigation = [
   },
 ];
 
-export const data = [
-  {
-    id: 1,
-    cover:
-      "https://images.unsplash.com/photo-1677352240737-595fbf4af29e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80",
-    title: "Business",
-    category: "Products",
-  },
-  {
-    id: 2,
-    cover:
-      "https://images.unsplash.com/photo-1677352240737-595fbf4af29e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80",
-    title: "Drugo",
-    category: "Web",
-  },
-  {
-    id: 2,
-    cover:
-      "https://images.unsplash.com/photo-1677352240737-595fbf4af29e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80",
-    title: "Drugo",
-    category: "Web",
-  },
-  {
-    id: 2,
-    cover:
-      "https://images.unsplash.com/photo-1677352240737-595fbf4af29e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80",
-    title: "Drug",
-    category: "Products",
-  },
-  {
-    id: 2,
-    cover:
-      "https://images.unsplash.com/photo-1677352240737-595fbf4af29e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80",
-    title: "Drugo",
-    category: "Web",
-  },
-];
+export const projectCategories = ["All", "React", "Vue", "Next", "React&Node"];
