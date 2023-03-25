0895d4a660cd9b5edfd2e82af9f6c5db0c7fa4b1
scokljat <selma.cokljat@hotmail.com>
2023-03-25 22:13:02 +0100

    Updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,18 @@
 import Image from "next/image";
 import classes from "./About.module.css";
 import { motion } from "framer-motion";
-function About() {
+import globalClasses from "../styles/Global.module.css";
+function About({ data }) {
+  console.log(data);
   return (
     <section className={classes.about} id="about">
-      <h1 className={classes.about_title}>About me</h1>
-      <hr className={classes.line} />
+      <h1 className={globalClasses.title}>About me</h1>
+      <hr className={globalClasses.title_line} />
       <div className={classes.about_container}>
-        <Image src={"/selma.jpg"} height={400} width={400} />
-        <div className={classes.about_info}>
-          <p>
-            Taken from the Latin words "dolorem ipsum", which translates to
-            "pain itself", Lorem Ipsum text saw a revival in the mid-20th
-            century as placeholder text in printing and design. Below, we've
-            shared the origins of Lorem Ipsum, why it's so important, and why
-            designers continue to use it today.{" "}
-          </p>
-          <button className={classes.button}>Download CV</button>
+        <Image src={"/selma.jpg"} height={300} width={250} />
+        <div className={classes.about_description}>
+          <h4>Education: {data.education}</h4>
+          <p>{data.aboutDescription}</p>
         </div>
       </div>
     </section>
